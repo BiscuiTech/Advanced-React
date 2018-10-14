2e26234758bba6d4177635ba57dce2cde387bec2
Jean-Cédric Huet <logit.pro@gmail.com>
2018-10-13 23:14:53 -0400

    Step 20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,29 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    console.log(item);
+    return item;
+  },
+  updateItem(parent, args, ctx, info){
+    //Take a copy of the updates
+    const updates = {...args};
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
+  }
+};
 
-module.exports = mutations;
+
+module.exports = Mutations;
